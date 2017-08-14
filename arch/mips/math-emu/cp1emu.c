eb8c73c757518a4f3433bd048596545f0ee009ea
Manuel Lauss <manuel.lauss@gmail.com>
2017-08-14 12:21:48 +0200

    MIPS: math-emu: do not use bools for arithmetic
    [ Upstream commit 8535f2ba0a9b971df62a5890699b9dfe2e0d5580 ]
    
    GCC-7 complains about a boolean value being used with an arithmetic
    AND:
    
    arch/mips/math-emu/cp1emu.c: In function 'cop1Emulate':
    arch/mips/math-emu/cp1emu.c:838:14: warning: '~' on a boolean expression [-Wbool-operation]
      fpr = (x) & ~(cop1_64bit(xcp) == 0);    \
                  ^
    arch/mips/math-emu/cp1emu.c:1068:3: note: in expansion of macro 'DITOREG'
       DITOREG(dval, MIPSInst_RT(ir));
       ^~~~~~~
    arch/mips/math-emu/cp1emu.c:838:14: note: did you mean to use logical not?
      fpr = (x) & ~(cop1_64bit(xcp) == 0);    \
    
    Since cop1_64bit() returns and int, just flip the LSB.
    
    Suggested-by: Maciej W. Rozycki <macro@imgtec.com>
    Signed-off-by: Manuel Lauss <manuel.lauss@gmail.com>
    Reviewed-by: Maciej W. Rozycki <macro@imgtec.com>
    Cc: linux-mips@linux-mips.org
    Patchwork: https://patchwork.linux-mips.org/patch/17058/
    Signed-off-by: Ralf Baechle <ralf@linux-mips.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- arch/mips/math-emu/cp1emu.c
+++ arch/mips/math-emu/cp1emu.c
@@ -834,7 +834,7 @@ do {									\
 #define DITOREG(di, x)							\
 do {									\
 	unsigned fpr, i;						\
-	fpr = (x) & ~(cop1_64bit(xcp) == 0);				\
+	fpr = (x) & ~(cop1_64bit(xcp) ^ 1);				\
 	set_fpr64(&ctx->fpr[fpr], 0, di);				\
 	for (i = 1; i < ARRAY_SIZE(ctx->fpr[x].val64); i++)		\
 		set_fpr64(&ctx->fpr[fpr], i, 0);			\
