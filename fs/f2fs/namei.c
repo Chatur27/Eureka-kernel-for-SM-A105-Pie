b312c595c37bb7103f89d379703d706e3b217350
Chatur27 <jasonbright2709@gmail.com>
2020-10-10 00:04:01 +0400

    f2fs: Fix build errors

--- fs/f2fs/namei.c
+++ fs/f2fs/namei.c
@@ -1207,30 +1207,6 @@ static const char *f2fs_encrypted_follow_link(struct dentry *dentry, void **cook
 	struct inode *inode = d_inode(dentry);
 	struct page *page;
 	void *target;
-	struct f2fs_encrypted_symlink_data *sd;
-	loff_t size = min_t(loff_t, i_size_read(inode), PAGE_SIZE - 1);
-	u32 max_size = inode->i_sb->s_blocksize;
-	int res;
-
-	res = f2fs_get_encryption_info(inode);
-	if (res)
-		return ERR_PTR(res);
-
-	cpage = read_mapping_page(inode->i_mapping, 0, NULL);
-	if (IS_ERR(cpage))
-		return ERR_CAST(cpage);
-	caddr = page_address(cpage);
-	caddr[size] = 0;
-
-	/* Symlink is encrypted */
-	sd = (struct f2fs_encrypted_symlink_data *)caddr;
-	cstr.len = le16_to_cpu(sd->len);
-	cstr.name = kmalloc(cstr.len, GFP_NOFS);
-	if (!cstr.name) {
-		res = -ENOMEM;
-		goto errout;
-	}
-	memcpy(cstr.name, sd->encrypted_path, cstr.len);
 
 	if (!dentry)
 		return ERR_PTR(-ECHILD);
@@ -1243,35 +1219,6 @@ static const char *f2fs_encrypted_follow_link(struct dentry *dentry, void **cook
 				     inode->i_sb->s_blocksize);
 	put_page(page);
 	return *cookie = target;
-	
-	if ((cstr.len + sizeof(struct f2fs_encrypted_symlink_data) - 1) >
-								max_size) {
-		/* Symlink data on the disk is corrupted */
-		res = -EIO;
-		goto errout;
-	}
-	res = f2fs_fname_crypto_alloc_buffer(inode, cstr.len, &pstr);
-	if (res)
-		goto errout;
-
-	res = f2fs_fname_disk_to_usr(inode, NULL, &cstr, &pstr);
-	if (res < 0)
-		goto errout;
-
-	kfree(cstr.name);
-
-	paddr = pstr.name;
-
-	/* Null-terminate the name */
-	paddr[res] = '\0';
-
-	page_cache_release(cpage);
-	return *cookie = paddr;
-errout:
-	kfree(cstr.name);
-	f2fs_fname_crypto_free_buffer(&pstr);
-	page_cache_release(cpage);
-	return ERR_PTR(res);
 }
 
 const struct inode_operations f2fs_encrypted_symlink_inode_operations = {
